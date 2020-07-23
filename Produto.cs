1f04927e47ddc576ad226a533ea976b0ce2006c4
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-07-23 15:46:11 -0300

    update produto.cs

--- Produto.cs
+++ Produto.cs
@@ -9,7 +9,7 @@
 
         public override string ToString()
         {
-            return $"Produto: {this.Id}, {this.Nome}, {this.Categoria}, {this.PrecoUnitario}";
+            return $"Produto: {this.Id}, {this.Nome}, {this.Categoria}, {this.Preco}";
         }
     }
 }
